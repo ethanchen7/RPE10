6859208391809c646ef09d1197d537fb0de71579
ethanchen7 <ethan.chen7@gmail.com>
2022-06-12 15:52:42 -0700

    splash page edits

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import NavBar from "./NavBar";
 import mac from "../../assets/images/rpe10-splash-mac.png";
 import iphone from "../../assets/images/transparentiphone.png";
@@ -10,9 +11,15 @@ const SplashPage = () => {
       <NavBar />
       <div className="splash-body-container">
         <div className="splash-body-header">
-          <h1>Put some clever header here</h1>
-          <h3>Some more stuff.</h3>
-          <h3>Welcome aboard RPE10</h3>
+          <h1>Train efficiently.</h1>
+          <h2>Ditch the spreadsheets.</h2>
+        </div>
+        <div className="get-started-btn-container">
+          <button className="splash-getstarted-btn">
+            <NavLink to="/login" exact={true} activeClassName="active">
+              Get Started
+            </NavLink>
+          </button>
         </div>
         <div className="splash-body-row-1">
           <img src={together} className="splash-mac" />
