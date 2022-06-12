ce74ee73b71699697a1a532e8ddfb05769492957
ethanchen7 <ethan.chen7@gmail.com>
2022-06-12 16:17:41 -0700

    splash edits

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -24,8 +24,27 @@ const SplashPage = () => {
         <div className="splash-body-row-1">
           <img src={together} className="splash-mac" />
           <div className="description-box-row-1">
-            <h3>Lol.</h3>
-            <p>Best project ever?</p>
+            <h3>Workout Programming</h3>
+            <p>
+              Plan out your training periodizations ahead of time and view your
+              workouts by block, week, and day.
+            </p>
+            <h3>Volume and RPE Tracking</h3>
+            <p>
+              Understand the load you are putting on your body with each
+              workout. See your average volume and RPE reports conveniently.
+              Adjust your workouts accordingly.
+            </p>
+            <h3>Evaluating Your Plan</h3>
+            <p>
+              Leave notes on each day for any thoughts you have on the workout,
+              things you want to keep in mind, or how to improve for next time.
+            </p>
+            <h3>Build a Community</h3>
+            <p>
+              Chat with other athletes over the messaging platform. Network with
+              coaches, trainers, and other like-minded individuals.
+            </p>
           </div>
         </div>
 
