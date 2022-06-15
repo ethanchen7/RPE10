15b224a3e5eef5777ea420cf00ad93be4293a834
ethanchen7 <ethan.chen7@gmail.com>
2022-06-15 16:33:27 -0700

    added backend form validations for large numbers of weight reps sets rpe

--- app/forms/exercise_form.py
+++ app/forms/exercise_form.py
@@ -4,10 +4,10 @@ from wtforms import StringField, IntegerField
 from wtforms.validators import Length, NumberRange
 
 class ExerciseForm(FlaskForm):
-    name = StringField('name', validators=[Length(max=60, message="Exercise name must be less than 60 characters")])
-    weight = IntegerField('weight',validators=[NumberRange(min=0, message="Cannot be negative.")])
-    sets = IntegerField('sets',validators=[NumberRange(min=0, message="Cannot be negative.")])
-    reps = IntegerField('reps',validators=[NumberRange(min=0, message="Cannot be negative.")])
-    rpe = IntegerField('rpe',validators=[NumberRange(min=0, message="Cannot be negative.")])
+    name = StringField('name', validators=[Length(max=60, message="Exercise name must be less than 60 characters.")])
+    weight = IntegerField('weight',validators=[NumberRange(min=0, message="Cannot be negative."), NumberRange(max=1500, message="You're not that strong. Weight must be <= 1500.")])
+    sets = IntegerField('sets',validators=[NumberRange(min=0, message="Cannot be negative."), NumberRange(max=20, message="Sets must be <= 20.")])
+    reps = IntegerField('reps',validators=[NumberRange(min=0, message="Cannot be negative."), NumberRange(max=100, message="Reps must be <= 100.")])
+    rpe = IntegerField('rpe',validators=[NumberRange(min=0, message="Cannot be negative."), NumberRange(max=10, message="RPE must be <= 10.")])
 
     total_vol = IntegerField("total_vol")
